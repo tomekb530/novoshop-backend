9e2e81773c91a076a09e9bfd0829b1d2124e76a4
tomekb530 <tomekb530@gmail.com>
2024-08-19 15:33:08 +0200

    feat: User login/register/email_confirm/password_reset

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -43,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    function hasRole(string $role)
+    {
+        return $this->role === $role || $this->role === 'superadmin';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "NovoShop Backend";
+});
+
+Route::get('/reset-password/{token}', function () {
+    return "NovoShop Backend";
+})->name('password.reset');
+
+Route::get('/images/logos/{filename}', function ($filename) {
+    $path = storage_path('app/logos/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response()->file($path);
 });
